7e6a8ded507c2f3c661893d6c1346f7da461359c
chexo7 <sergio.acevedo.santic@gmail.com>
2025-09-01 11:14:38 -0400

    Add PCSWMM export option

--- components/ExportModal.tsx
+++ components/ExportModal.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 interface ExportModalProps {
   onExportHydroCAD: () => void;
+  onExportPCSWMM: () => void;
   onExportShapefiles: () => void;
   onClose: () => void;
   exportEnabled?: boolean;
 }
 
-const ExportModal: React.FC<ExportModalProps> = ({ onExportHydroCAD, onExportShapefiles, onClose, exportEnabled }) => {
+const ExportModal: React.FC<ExportModalProps> = ({ onExportHydroCAD, onExportPCSWMM, onExportShapefiles, onClose, exportEnabled }) => {
   return (
     <div className="fixed inset-0 bg-black/60 flex items-center justify-center z-[2000]">
       <div className="bg-gray-800 p-6 rounded-lg border border-gray-600 w-80 space-y-4">
@@ -24,6 +25,16 @@ const ExportModal: React.FC<ExportModalProps> = ({ onExportHydroCAD, onExportSha
           }
         >
           Export to HydroCAD
+        </button>
+        <button
+          onClick={onExportPCSWMM}
+          disabled={!exportEnabled}
+          className={
+            'w-full font-semibold px-4 py-2 rounded ' +
+            (exportEnabled ? 'bg-cyan-600 hover:bg-cyan-700 text-white' : 'bg-gray-600 text-gray-300 cursor-not-allowed')
+          }
+        >
+          Export to PCSWMM
         </button>
         <button
           onClick={onExportShapefiles}
